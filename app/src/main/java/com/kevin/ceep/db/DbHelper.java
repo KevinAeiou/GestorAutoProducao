0cf4c86a5b7b2408342a99f920a620387c9342db
KevinAeiou <kevin.souzaeiou@outlook.com>
2024-12-24 17:37:59 -0400

    Refatorar a classe DbHelper

--- app/src/main/java/com/kevin/ceep/db/DbHelper.java
+++ app/src/main/java/com/kevin/ceep/db/DbHelper.java
@@ -1,15 +1,21 @@
 package com.kevin.ceep.db;
 
+import static com.kevin.ceep.db.contracts.EstoqueDbContract.EstoqueEntry.TABLE_ESTOQUE;
+import static com.kevin.ceep.db.contracts.PersoagemDbContract.PersonagemEntry.TABLE_PERSONAGENS;
+import static com.kevin.ceep.db.contracts.TrabalhoDbContract.TrabalhoEntry.TABLE_TRABALHOS;
+import static com.kevin.ceep.db.contracts.TrabalhoProducaoContract.TrabalhoProducaoEntry.TABLE_TRABALHOS_PRODUCAO;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 import com.kevin.ceep.db.contracts.EstoqueDbContract.EstoqueEntry;
 import com.kevin.ceep.db.contracts.PersoagemDbContract.PersonagemEntry;
 import com.kevin.ceep.db.contracts.TrabalhoDbContract.TrabalhoEntry;
 import com.kevin.ceep.db.contracts.TrabalhoProducaoContract.TrabalhoProducaoEntry;
 
 public class DbHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 5;
+    public static final int DATABASE_VERSION = 6;
     public static final String DATABASE_NAME = "autoProducao.db";
     private static DbHelper minhaInstancia = null;
 
@@ -26,7 +32,7 @@ public class DbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(
-                "CREATE TABLE " + TrabalhoEntry.TABLE_NAME + " (" +
+                "CREATE TABLE " + TABLE_TRABALHOS + " (" +
                 TrabalhoEntry.COLUMN_NAME_ID + " VARCHAR(30) PRIMARY KEY," +
                 TrabalhoEntry.COLUMN_NAME_NOME + " TEXT," +
                 TrabalhoEntry.COLUMN_NAME_NOME_PRODUCAO + " TEXT," +
@@ -38,7 +44,7 @@ public class DbHelper extends SQLiteOpenHelper {
                 ")"
         );
         sqLiteDatabase.execSQL(
-                "CREATE TABLE " + PersonagemEntry.TABLE_NAME + " (" +
+                "CREATE TABLE " + TABLE_PERSONAGENS + " (" +
                 PersonagemEntry.COLUMN_NAME_ID + " VARCHAR(30) PRIMARY KEY," +
                 PersonagemEntry.COLUMN_NAME_ID_USUARIO + " VARCHAR(30)," +
                 PersonagemEntry.COLUMN_NAME_NOME + " TEXT," +
@@ -51,7 +57,7 @@ public class DbHelper extends SQLiteOpenHelper {
         );
         sqLiteDatabase.execSQL(
                 "CREATE TABLE " +
-                TrabalhoProducaoEntry.TABLE_NAME +
+                TABLE_TRABALHOS_PRODUCAO +
                 " (" +
                 TrabalhoProducaoEntry.COLUMN_NAME_ID + " VARCHAR(30) PRIMARY KEY," +
                 TrabalhoProducaoEntry.COLUMN_NAME_ID_TRABALHO + " VARCHAR(30)," +
@@ -63,7 +69,7 @@ public class DbHelper extends SQLiteOpenHelper {
         );
         sqLiteDatabase.execSQL(
                 "CREATE TABLE " +
-                EstoqueEntry.TABLE_NAME +
+                TABLE_ESTOQUE +
                 " (" +
                 EstoqueEntry.COLUMN_NAME_ID + " VARCHAR(30) PRIMARY KEY," +
                 EstoqueEntry.COLUMN_NAME_ID_TRABALHO + " VARCHAR(30)," +
@@ -75,10 +81,10 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TrabalhoEntry.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + PersonagemEntry.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TrabalhoProducaoEntry.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + EstoqueEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TRABALHOS);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_PERSONAGENS);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TRABALHOS_PRODUCAO);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_ESTOQUE);
         onCreate(sqLiteDatabase);
     }
 }
