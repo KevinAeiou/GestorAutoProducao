8dcd3bcd19b3baacac091a419259482c055f3af1
KevinAeiou <kevin.souzaeiou@outlook.com>
2024-12-24 18:05:05 -0400

    Implementar a função criaUsuario e insereUsuario

--- app/src/main/java/com/kevin/ceep/repository/FirebaseAuthRepository.java
+++ app/src/main/java/com/kevin/ceep/repository/FirebaseAuthRepository.java
@@ -1,30 +1,75 @@
 package com.kevin.ceep.repository;
 
+import static com.kevin.ceep.ui.activity.NotaActivityConstantes.CHAVE_USUARIOS;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthEmailException;
-import com.google.rpc.context.AttributeContext;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.kevin.ceep.model.Usuario;
+
+import java.util.Objects;
 
 public class FirebaseAuthRepository {
-    private FirebaseAuth minhaInstancia;
+    private final FirebaseAuth minhaInstancia;
+    private final DatabaseReference minhaReferencia;
 
-    public FirebaseAuthRepository(FirebaseAuth minhaInstancia) {
-        this.minhaInstancia = minhaInstancia;
+    public FirebaseAuthRepository() {
+        this.minhaInstancia = FirebaseAuth.getInstance();
+        this.minhaReferencia = FirebaseDatabase.getInstance().getReference(CHAVE_USUARIOS);
+    }
+    public LiveData<Resource<Void>> autenticarUsuario(Usuario usuario) {
+        MutableLiveData<Resource<Void>> liveData = new MutableLiveData<>();
+        minhaInstancia.signInWithEmailAndPassword(usuario.getEmail(),usuario.getSenha())
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    liveData.setValue(new Resource<>(null, null));
+                } else {
+                    liveData.setValue(new Resource<>(null, Objects.requireNonNull(task.getException()).getMessage()));
+                }
+            });
+        return liveData;
     }
 
-    public boolean autenticarUsuario(String email, String senha) {
-        final boolean[] confirmacao = {false};
-        minhaInstancia.signInWithEmailAndPassword(email,senha)
+    public LiveData<Resource<Void>> criaUsuario(Usuario usuario) {
+        MutableLiveData<Resource<Void>> liveData = new MutableLiveData<>();
+        minhaInstancia.createUserWithEmailAndPassword(usuario.getEmail(),usuario.getSenha())
                 .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        confirmacao[0] = true;
-                    } else {
-                        confirmacao[0] = false;
-                        Exception exception = task.getException();
-                        if (exception != null) {}
+                    if (task.isSuccessful()){
+                        liveData.setValue(new Resource<>(null, null));
+                    }else{
+                        String erro;
+                        try{
+                            throw Objects.requireNonNull(task.getException());
+                        }catch (FirebaseAuthWeakPasswordException e){
+                            erro = "A senha deve conter no mínimo 8 caracteres!";
+                        } catch (FirebaseAuthUserCollisionException e){
+                            erro = "Conta já cadastrada!";
+                        }catch (FirebaseAuthInvalidCredentialsException e) {
+                            erro = "Email inválido!";
+                        }catch (Exception e) {
+                            erro = "Erro ao cadastrar usuário";
+                        }
+                        liveData.setValue(new Resource<>(null, erro));
                     }
                 });
-        return confirmacao[0];
+        return liveData;
+    }
+
+    public LiveData<Resource<Void>> insereUsuario(Usuario usuario) {
+        MutableLiveData<Resource<Void>> liveData = new MutableLiveData<>();
+        minhaReferencia.child(usuario.getId()).setValue(usuario).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                liveData.setValue(new Resource<>(null, null));
+            } else {
+                liveData.setValue(new Resource<>(null, Objects.requireNonNull(task.getException()).getMessage()));
+            }
+        });
+        return liveData;
     }
 }
