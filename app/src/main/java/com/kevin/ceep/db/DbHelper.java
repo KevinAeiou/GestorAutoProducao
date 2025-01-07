d7d07a415430e0cd81ec128f1f14ee7351c989f0
Kevin <kevin.souzaeiou@outlook.com>
2025-01-07 14:21:03 -0400

    :sparkles: feat: Atualizar versão do banco, criar tabela Lista_vendas

--- app/src/main/java/com/kevin/ceep/db/DbHelper.java
+++ app/src/main/java/com/kevin/ceep/db/DbHelper.java
@@ -1,21 +1,28 @@
 package com.kevin.ceep.db;
 
+import static com.kevin.ceep.db.contracts.EstoqueDbContract.EstoqueEntry.COLUMN_NAME_ID;
+import static com.kevin.ceep.db.contracts.EstoqueDbContract.EstoqueEntry.COLUMN_NAME_ID_PERSONAGEM;
+import static com.kevin.ceep.db.contracts.EstoqueDbContract.EstoqueEntry.COLUMN_NAME_ID_TRABALHO;
+import static com.kevin.ceep.db.contracts.EstoqueDbContract.EstoqueEntry.COLUMN_NAME_QUANTIDADE;
 import static com.kevin.ceep.db.contracts.EstoqueDbContract.EstoqueEntry.TABLE_ESTOQUE;
 import static com.kevin.ceep.db.contracts.PersoagemDbContract.PersonagemEntry.TABLE_PERSONAGENS;
 import static com.kevin.ceep.db.contracts.TrabalhoDbContract.TrabalhoEntry.TABLE_TRABALHOS;
 import static com.kevin.ceep.db.contracts.TrabalhoProducaoContract.TrabalhoProducaoEntry.TABLE_TRABALHOS_PRODUCAO;
+import static com.kevin.ceep.db.contracts.TrabalhoVendidoContract.TrabalhoVendidoEntry.COLUMN_NAME_DATA_VENDA;
+import static com.kevin.ceep.db.contracts.TrabalhoVendidoContract.TrabalhoVendidoEntry.COLUMN_NAME_DESCRICAO;
+import static com.kevin.ceep.db.contracts.TrabalhoVendidoContract.TrabalhoVendidoEntry.COLUMN_NAME_VALOR;
+import static com.kevin.ceep.db.contracts.TrabalhoVendidoContract.TrabalhoVendidoEntry.TABLE_TRABALHOS_VENDIDOS;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.kevin.ceep.db.contracts.EstoqueDbContract.EstoqueEntry;
 import com.kevin.ceep.db.contracts.PersoagemDbContract.PersonagemEntry;
 import com.kevin.ceep.db.contracts.TrabalhoDbContract.TrabalhoEntry;
 import com.kevin.ceep.db.contracts.TrabalhoProducaoContract.TrabalhoProducaoEntry;
 
 public class DbHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 6;
+    public static final int DATABASE_VERSION = 8;
     public static final String DATABASE_NAME = "autoProducao.db";
     private static DbHelper minhaInstancia = null;
 
@@ -71,10 +78,23 @@ public class DbHelper extends SQLiteOpenHelper {
                 "CREATE TABLE " +
                 TABLE_ESTOQUE +
                 " (" +
-                EstoqueEntry.COLUMN_NAME_ID + " VARCHAR(30) PRIMARY KEY," +
-                EstoqueEntry.COLUMN_NAME_ID_TRABALHO + " VARCHAR(30)," +
-                EstoqueEntry.COLUMN_NAME_ID_PERSONAGEM + " VARCHAR(30)," +
-                EstoqueEntry.COLUMN_NAME_QUANTIDADE + " INTEGER" +
+                COLUMN_NAME_ID + " VARCHAR(30) PRIMARY KEY," +
+                COLUMN_NAME_ID_TRABALHO + " VARCHAR(30)," +
+                COLUMN_NAME_ID_PERSONAGEM + " VARCHAR(30)," +
+                COLUMN_NAME_QUANTIDADE + " INTEGER" +
+                ")"
+        );
+        sqLiteDatabase.execSQL(
+                "CREATE TABLE " +
+                TABLE_TRABALHOS_VENDIDOS +
+                " (" +
+                COLUMN_NAME_ID + " VARCHAR(30) PRIMARY KEY," +
+                COLUMN_NAME_ID_PERSONAGEM + " VARCHAR(30), " +
+                COLUMN_NAME_ID_TRABALHO + " VARCHAR(30), " +
+                COLUMN_NAME_DESCRICAO + " TEXT, " +
+                COLUMN_NAME_DATA_VENDA + " VARCHAR(10), " +
+                COLUMN_NAME_QUANTIDADE + " INTEGER, " +
+                COLUMN_NAME_VALOR + " INTEGER" +
                 ")"
         );
     }
@@ -85,6 +105,7 @@ public class DbHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_PERSONAGENS);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TRABALHOS_PRODUCAO);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_ESTOQUE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TRABALHOS_VENDIDOS);
         onCreate(sqLiteDatabase);
     }
 }
