c16c891922fce474ebd922034494ff96ba60aa70
Kevin <kevin.souzaeiou@outlook.com>
2025-01-07 13:47:13 -0400

    :sparkles: feat: interface OnItemClickListenerTrabalhoVendido

---
+++ app/src/main/java/com/kevin/ceep/ui/recyclerview/adapter/listener/OnItemClickListenerTrabalhoVendido.java
@@ -0,0 +1,7 @@
+package com.kevin.ceep.ui.recyclerview.adapter.listener;
+
+import com.kevin.ceep.model.TrabalhoVendido;
+
+public interface OnItemClickListenerTrabalhoVendido {
+    void onItemClick(TrabalhoVendido trabalhoVendido);
+}
