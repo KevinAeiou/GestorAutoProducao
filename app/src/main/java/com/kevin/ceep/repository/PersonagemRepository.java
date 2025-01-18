6fc3e69c662ee888cd0eaf9d5339fd68e61b5ef6
KevinAeiou <kevin.souzaeiou@outlook.com>
2025-01-18 14:30:16 -0300

    :recycle: refactor: função pegaTodosPersonagens

--- app/src/main/java/com/kevin/ceep/repository/PersonagemRepository.java
+++ app/src/main/java/com/kevin/ceep/repository/PersonagemRepository.java
@@ -154,16 +154,16 @@ public class PersonagemRepository {
         );
         ArrayList<Personagem> personagens = new ArrayList<>();
         while (cursor.moveToNext()) {
-            boolean estado = cursor.getInt(5) == 1;
-            boolean uso = cursor.getInt(6) == 1;
+            boolean estado = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_ESTADO)) == 1;
+            boolean uso = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_USO)) == 1;
             Personagem personagem = new Personagem ();
-            personagem.setId(cursor.getString(0));
-            personagem.setNome(cursor.getString(2));
-            personagem.setEmail(cursor.getString(3));
-            personagem.setSenha(cursor.getString(4));
+            personagem.setId(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME_ID)));
+            personagem.setNome(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME_NOME)));
+            personagem.setEmail(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME_EMAIL)));
+            personagem.setSenha(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME_SENHA)));
             personagem.setEstado(estado);
             personagem.setUso(uso);
-            personagem.setEspacoProducao(cursor.getInt(7));
+            personagem.setEspacoProducao(cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_NAME_ESPACO_PRODUCAO)));
             personagens.add(personagem);
         }
         cursor.close();
